cde2630eeb32990057a3dbff33eee8f5e0f2b75b
Pranav Ladkat <pranavladkat@gmail.com>
2015-01-05 23:53:46 -0500

    comments added

--- convert_file.sh
+++ convert_file.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# cd to folder with images saved by screenshot function
 cd Re5Bo1
 
+# convert eps to png
 for f in `ls *.eps`; do
      convert -density 200 $f -flatten ${f%.*}.png;
 done
 
+#build movie
 mencoder "mf://*.png" -mf fps=10 -o output.avi -ovc lavc -lavcopts vcodec=mpeg4
 
+#remove images
 rm *.eps
-#rm *.png
+rm *.png
